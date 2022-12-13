0797d984a9fda9c40e478ac9698a9eb79c811e32
Jack Morrell <jack.morrell79@googlemail.com>
2022-12-13 11:16:18 +0100

    update js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const navButton = document.querySelector(".btn-mobile-nav");
 
 const shade = document.querySelector(".page-mask");
 
-navButton.addEventListener("click", function () {
+navButton.addEventListener("click", function (e) {
+	e.preventDefault();
 	header.classList.toggle("nav-open");
 	// shade.classList.toggle("darken");
 });
